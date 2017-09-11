1bd5ca5d68c16cd3b7daa66bc38497f13b276243
Benjy96 <benjamindickey@hotmail.co.uk>
2017-09-11 15:10:29 +0100

    Create state base class for owned states

---
+++ UnityAndroid1/Assets/Scripts/States/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// State base class
+/// </summary>
+/// <typeparam name="T">The entity which will be using the states</typeparam>
+public abstract class State<T>
+{
+    public abstract void Enter(ref T Entity);
+
+    public abstract void Execute(ref T Entity);
+
+    public abstract void Exit(ref T Entity);
+
+    public abstract bool OnMessage(ref T Entity);
+}
