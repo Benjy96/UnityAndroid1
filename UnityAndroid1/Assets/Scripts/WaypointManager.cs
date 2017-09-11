5babe5fbe7e9bd9e1b2b6fb872b6e1e241344a0f
Benjy96 <benjamindickey@hotmail.co.uk>
2017-09-11 13:10:54 +0100

    Add waypoint manager class

--- UnityAndroid1/Assets/Scripts/WaypointManager.cs
+++ UnityAndroid1/Assets/Scripts/WaypointManager.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// With this class I want to assign waypoints along the board, like the way you would assign tiles to a 2D game with a board manager.
+/// I will get the size of the board, then loop through the board, disregarding spaces taken up through the board (by gameobjects) and place waypoints for the guards to follow.
+/// </summary>
 public class WaypointManager : MonoBehaviour {
 
     //Board and its size along horizontal/vertical
@@ -13,6 +17,7 @@ public class WaypointManager : MonoBehaviour {
 	void Start () {
         boardX = board.transform.localScale.x;
         boardZ = board.transform.localScale.z;
+
     }
 	
 	// Update is called once per frame
