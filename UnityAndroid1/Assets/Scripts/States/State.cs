2d54eb4fc63218c3b392556f40795def528c6175
Benjy96 <benjamindickey@hotmail.co.uk>
2017-09-11 16:39:42 +0100

    Generics explanation

--- UnityAndroid1/Assets/Scripts/States/State.cs
+++ UnityAndroid1/Assets/Scripts/States/State.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// State base class
+/// State base class.
+/// If I weren't using a generic, I would have to create a separate base class for each type of entity to inherit from. e.g. the abstract method params would be means 3 diff files for guard, specialGuard, ProGuard, etc.
 /// </summary>
 /// <typeparam name="entity_type">The entity which will be using the states. Means each method here can access public interface of the caller. e.g. if Guard calls execute, the execute can do entity.x();</typeparam>
 public abstract class State<entity_type>
