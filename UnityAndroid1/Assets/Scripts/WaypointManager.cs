7bf59dda052bf434748694d9a4472e51f846f3ca
Benjy96 <benjamindickey@hotmail.co.uk>
2017-09-11 13:48:25 +0100

    Fills half of board

--- UnityAndroid1/Assets/Scripts/WaypointManager.cs
+++ UnityAndroid1/Assets/Scripts/WaypointManager.cs
@@ -23,16 +23,21 @@ public class WaypointManager : MonoBehaviour {
         boardX = board.transform.localScale.x;
         boardZ = board.transform.localScale.z;
 
+        //Adjust scale to co-ordinates in world
+        boardX *= 5;
+        boardZ *= 5;
+
         //Need to fill the board with waypoints
         //Unity large squares are made up of 10 smaller squares
         //e.g. one x = 10 small inside it. Let's put 10 waypoints per 1 on x/y/z axis.
 
         //Fill the board with waypoints - we want to increment in tenths of X
-        for(float i = 0; i <= boardX; i+=.1f)
+        for(int i = 0; i <= boardX; i++)
         {
-            for(float j = 0; j <= boardZ; j+=.1f)
+            for(int j = 0; j <= boardZ; j++)
             {
                 //Place waypoints at a corrected height and place into an array for later use?
+                print(i);
                 Instantiate(waypoint, new Vector3(i, 0.5f, j), Quaternion.identity, pathHolder);
             }
         }
