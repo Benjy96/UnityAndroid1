087d070e7c68ab2f00cfc55a4421f3b2da329ed0
Benjy96 <benjamindickey@hotmail.co.uk>
2017-09-11 14:17:04 +0100

    Organise code layout

--- UnityAndroid1/Assets/Scripts/WaypointManager.cs
+++ UnityAndroid1/Assets/Scripts/WaypointManager.cs
@@ -19,6 +19,16 @@ public class WaypointManager : MonoBehaviour {
     public Transform waypoint;
 
 	void Start () {
+        PlaceWaypoints();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void PlaceWaypoints()
+    {
         //Get size of board
         boardX = board.transform.localScale.x;
         boardZ = board.transform.localScale.z;
@@ -29,14 +39,10 @@ public class WaypointManager : MonoBehaviour {
         boardX--;
         boardZ--;
 
-        //Need to fill the board with waypoints
-        //Unity large squares are made up of 10 smaller squares
-        //e.g. one x = 10 small inside it. Let's put 10 waypoints per 1 on x/y/z axis.
-
         //Fill top left quadrant
-        for(int i = 0; i <= boardX; i+=5)
+        for (int i = 0; i <= boardX; i += 5)
         {
-            for(int j = 0; j <= boardZ; j+=5)
+            for (int j = 0; j <= boardZ; j += 5)
             {
                 //Place waypoints at a corrected height and place into an array for later use?
                 Instantiate(waypoint, new Vector3(i, 0.5f, j), Quaternion.identity, pathHolder);
@@ -44,35 +50,30 @@ public class WaypointManager : MonoBehaviour {
         }
 
         //Fill bottom left quadrant
-        for(int i = 0; i >= -boardX; i -= 5)
+        for (int i = 0; i >= -boardX; i -= 5)
         {
             for (int j = 0; j <= boardZ; j += 5)
             {
                 Instantiate(waypoint, new Vector3(i, 0.5f, j), Quaternion.identity, pathHolder);
             }
         }
-        
+
         //Fill top right quadrant
-        for(int i = 0; i < boardX; i += 5)
+        for (int i = 0; i < boardX; i += 5)
         {
-            for(int j = 0; j >= -boardZ; j -= 5)
+            for (int j = 0; j >= -boardZ; j -= 5)
             {
                 Instantiate(waypoint, new Vector3(i, 0.5f, j), Quaternion.identity, pathHolder);
             }
         }
 
         //Fill bottom right quadrant
-        for(int i = 0; i >= -boardX; i -= 5)
+        for (int i = 0; i >= -boardX; i -= 5)
         {
-            for(int j = 0; j >= -boardZ; j -= 5)
+            for (int j = 0; j >= -boardZ; j -= 5)
             {
                 Instantiate(waypoint, new Vector3(i, 0.5f, j), Quaternion.identity, pathHolder);
             }
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
