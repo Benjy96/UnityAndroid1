e5e7d981cf61d030b6dc25b9b463ec9f681073d0
Benjy96 <benjamindickey@hotmail.co.uk>
2017-09-12 11:52:30 +0100

    Register spawn points
    In BoardManager I have created a list which will store potential spawn points. We can then remove spawn points which have been spawned on, preventing any duplicates/multiple spawns on the one location.

--- UnityAndroid1/Assets/Scripts/BoardManager.cs
+++ UnityAndroid1/Assets/Scripts/BoardManager.cs
@@ -15,6 +15,9 @@ public class BoardManager : MonoBehaviour {
     private float boardX;
     private float boardZ;
 
+    //Store placed object locations
+    private List<Vector3> spawnPositions;
+
     //Waypoint Holder
     public Transform pathHolder;
     //Waypoint prefab
@@ -22,11 +25,15 @@ public class BoardManager : MonoBehaviour {
 
     void Start()
     {
+        spawnPositions = new List<Vector3>();
+
+
+
         //Need code to place player and walls before the waypoints, and mark their position so that waypoint skips over them when looping
-        PlaceObjects(waypoint);
+        PlaceObjects(waypoint, .5f);
     }
 
-    private void PlaceObjects(Transform toPlace)
+    private void PlaceObjects(Transform toPlace, float yAxisHeight)
     {
         //Get size of board
         boardX = board.transform.localScale.x;
@@ -44,7 +51,7 @@ public class BoardManager : MonoBehaviour {
             for (int j = 0; j <= boardZ; j += 5)
             {
                 //Place waypoints at a corrected height and place into an array for later use?
-                Instantiate(toPlace, new Vector3(i, .5f, j), Quaternion.identity, pathHolder);
+                Instantiate(toPlace, new Vector3(i, yAxisHeight, j), Quaternion.identity, pathHolder);
             }
         }
 
@@ -53,7 +60,7 @@ public class BoardManager : MonoBehaviour {
         {
             for (int j = 0; j <= boardZ; j += 5)
             {
-                Instantiate(toPlace, new Vector3(i, .5f, j), Quaternion.identity, pathHolder);
+                Instantiate(toPlace, new Vector3(i, yAxisHeight, j), Quaternion.identity, pathHolder);
             }
         }
 
@@ -62,7 +69,7 @@ public class BoardManager : MonoBehaviour {
         {
             for (int j = 0; j >= -boardZ; j -= 5)
             {
-                Instantiate(toPlace, new Vector3(i, .5f, j), Quaternion.identity, pathHolder);
+                Instantiate(toPlace, new Vector3(i, yAxisHeight, j), Quaternion.identity, pathHolder);
             }
         }
 
@@ -71,8 +78,50 @@ public class BoardManager : MonoBehaviour {
         {
             for (int j = 0; j >= -boardZ; j -= 5)
             {
-                Instantiate(toPlace, new Vector3(i, .5f, j), Quaternion.identity, pathHolder);
+                Instantiate(toPlace, new Vector3(i, yAxisHeight, j), Quaternion.identity, pathHolder);
             }
         }
     }
+
+    //what if we loop through a list and every entry is a possible spawn point? then we can REMOVE the spawn points from the list with a list's functionality
+    private void MarkSpawnPoints()
+    {
+        //Fill top left quadrant
+        for (int i = 0; i <= boardX; i += 5)
+        {
+            for (int j = 0; j <= boardZ; j += 5)
+            {
+                spawnPositions.Add(new Vector3(i, 0f, j));
+            }
+        }
+
+        //Fill bottom left quadrant
+        for (int i = 0; i >= -boardX; i -= 5)
+        {
+            for (int j = 0; j <= boardZ; j += 5)
+            {
+                spawnPositions.Add(new Vector3(i, 0f, j));
+            }
+        }
+
+        //Fill top right quadrant
+        for (int i = 0; i < boardX; i += 5)
+        {
+            for (int j = 0; j >= -boardZ; j -= 5)
+            {
+                spawnPositions.Add(new Vector3(i, 0f, j));
+            }
+        }
+
+        //Fill bottom right quadrant
+        for (int i = 0; i >= -boardX; i -= 5)
+        {
+            for (int j = 0; j >= -boardZ; j -= 5)
+            {
+                spawnPositions.Add(new Vector3(i, 0f, j));
+            }
+        }
+    }
+
+
 }
